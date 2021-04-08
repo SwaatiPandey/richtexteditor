9ee716644366700bf5bac09b026cf5384bdcc893
SwaatiPandey <swati.pandey@contentstack.com>
2021-04-08 13:32:17 +0530

    improved dnd code

--- src/components/dndBlock/dndBlock.js
+++ src/components/dndBlock/dndBlock.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { useDrag, useDrop } from "react-dnd";
 import { Transforms } from "slate";
 import { ReactEditor, useEditor } from "slate-react";
@@ -12,46 +12,25 @@ const DNDBlock = (props) => {
     backgroundColor: "white",
     display: "flex",
   };
-  const ref = useRef(null);
   const { element } = props;
-  const [, drop] = useDrop({
-    accept: "DNDBlock",
-    hover(item, monitor) {
-      if (!ref.current) {
-        return;
-      }
-      const dragIndex = ReactEditor.findPath(editor, item.element);
-      const hoverIndex = ReactEditor.findPath(editor, element);
-      console.log("for drag", dragIndex);
-      console.log("for hover", hoverIndex);
-    },
-    drop(item, monitor) {
-      // console.log(item.test);
-      while (item.test !== true) {
-        // console.log(item);
-        const dragIndex = ReactEditor.findPath(editor, item.element);
-        const hoverIndex = ReactEditor.findPath(editor, element);
-        if (dragIndex === hoverIndex) {
-          return;
-        }
-        moveItem(dragIndex, hoverIndex);
-        item.test = true;
-      }
-    },
-  });
-
-  const moveItem = (dragIndex, hoverIndex) => {
-    Transforms.moveNodes(editor, { at: dragIndex, to: hoverIndex });
-  };
-
   const [{ opacity }, drag, preview] = useDrag(() => ({
     item: { type: "DNDBlock", element: element },
     collect: (monitor) => ({
       opacity: monitor.isDragging() ? 0.4 : 1,
     }),
   }));
-  // console.log(props);
-  // console.log(props.element.type);
+
+  const [, drop] = useDrop({
+    accept: "DNDBlock",
+    drop(item, monitor) {
+      {
+        const dragIndex = ReactEditor.findPath(editor, item.element);
+        const hoverIndex = ReactEditor.findPath(editor, element);
+        Transforms.moveNodes(editor, { at: dragIndex, to: hoverIndex });
+      }
+    },
+  });
+
   return (
     <>
       {props.element.type === "link" ? (
@@ -67,7 +46,6 @@ const DNDBlock = (props) => {
             <div
               ref={drop}
               style={{ display: "flex", width: "100%", outline: "none" }}
-              onDrag={(event) => event.stopPropagation()}
             >
               <div
                 contentEditable={false}
